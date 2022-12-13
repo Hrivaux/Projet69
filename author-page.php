6a4a17f65de6144bbb164e7ab59f5f5af37f7b91
Hrivaux <hugorivaux@gmail.com>
2022-12-13 17:49:02 +0100

    fe

--- author-page.php
+++ author-page.php
@@ -37,7 +37,7 @@ include('templates/meta.php');
                                 <div class="card-body d-block pt-4 text-center position-relative">
                                     <figure class="avatar mt--6 position-relative w75 z-index-1 w100 z-index-1 ms-auto me-auto"><img src="https://via.placeholder.com/100x100.png" alt="image" class="p-1 bg-white rounded-xl w-100"></figure>
 
-                                    <h4 class="font-xs ls-1 fw-700 text-grey-900"><?php echo $prenomnom ?><span class="d-block font-xssss fw-500 mt-1 lh-3 text-grey-500"><?php echo $profilconnecte['identifiant']; ?></span></h4>
+                                    <h4 class="font-xs ls-1 fw-700 text-grey-900"><?php echo $prenomnom ?><span class="d-block font-xssss fw-500 mt-1 lh-3 text-grey-500"><?php echo $profilconnecte['pseudo']; ?></span></h4>
                                     
                                     <div class="d-flex align-items-center pt-0 position-absolute left-15 top-10 mt-4 ms-2">
                                         <h4 class="font-xsssss text-center d-none d-lg-block text-grey-500 fw-600 ms-2 me-2"><b class="text-grey-900 mb-1 font-sm fw-700 d-inline-block ls-3 text-dark">456 </b> Posts</h4>
